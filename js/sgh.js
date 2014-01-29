65128aa0034637918aafca3de09d8e4168a7152d
jdungan <johnadungan@gmail.com>
2014-01-29 06:46:39 -0600

    add jqm

--- js/sgh.js
+++ js/sgh.js
@@ -1,9 +1,24 @@
+"use strict";
 
+    function resizeAll() {
+      d3.select(this[0][0])
+        .attr('width', window.innerWidth)
+        .attr('height', window.innerHeight)
+    }
 
+    $("#data_switch").on("pagecreate", function() {
 
+        var svg = d3.select('#data_switch').append('svg');
 
-d3.select('body')
-.append('div')
-.attr('id','location_score')
-.append('div')
-.attr('id','decoder_ring')
+        var control = svg.append('rect')
+
+        control.attr('x', 0)
+          .attr('y', 0)
+          .attr('fill', 'blue');
+
+          control.resize = resizeAll;
+
+          control.resize();
+
+    });
+
