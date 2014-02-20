404232183eea2b1c00ae5575ea038e27f207f203
jdungan <johnadungan@gmail.com>
2014-02-19 19:20:36 -0600

    add score base on wheel rotation

--- js/sgh.js
+++ js/sgh.js
@@ -1,114 +1,177 @@
 "use strict";
 
-  var circles =[  
-    {label:'Howdy',score:50},
-    {label:'Dammit',score:55},
-    {label:'Gigem',score:52},
-    {label:'Aggies',score:70},
-    {label:'Rock',score:150},
-    {label:'Good',score:530},
-    {label:'Bull',score:84}
-  ]
+var circles = [{
+  label: 'Gigem',
+  score: 80
+}, {
+  label: 'Aggies',
+  score: 70
+}, {
+  label: 'Howdy',
+  score: 62
+}, {
+  label: 'Dammit',
+  score: 55
+}, ]
 
 
-    $("#data_switch").on("pagecreate", function() {
+$("#data_switch").on("pagecreate", function() {
 
-      var svg = d3.select('#canvas')
-        .append('svg')
-          .attr('width',window.innerWidth)
-          .attr('height',window.innerHeight)
+  var svg = d3.select('#canvas')
+    .append('svg')
+    .attr('width', window.innerWidth)
+    .attr('height', window.innerHeight)
 
 
-      var twoPI = Math.PI*2
+  var interval = 2 * Math.PI / circles.length
+
+  var radius = 100;
+
+  var color = d3.scale.category10();
+
+  var arc2deg = function(x) {
+    return x != 0 ? 180 * (x / Math.PI) : 0
+  }
+
+
+  var transformation = function(options) {
+    this.options = options;
+    this.text = function() {
+      options = this.options;
+      return 'translate(' + options.translate.x + ',' + options.translate.y + ') scale(' + options.scale.x + ',' + options.scale.y + ') rotate(' + options.rotate + ')'
+    }
+  }
+
+  var wheel_options = {
+    translate: {
+      x: window.innerWidth / 2,
+      y: window.innerHeight
+    },
+    scale: {
+      x: 4,
+      y: 2
+    },
+    rotate: 0
+  }
+
+
+  var score_options = {
+    translate: {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 4
+    },
+    scale: {
+      x: 5,
+      y: 5
+    },
+    rotate: 0
+  }
+
+  var scores = d3.scale.quantize()
+    .domain([0,100])
+    .range(['F','F','F','F','F','D','C','B','A']);
+  
+  var wheel_position = d3.scale.quantize()
+  .domain([360,0])
+  .range(circles)
+  
+ 
+  var score = svg.append('g')
+    .attr('id', 'score')
+    .append('text')
+    .text(scores(circles[0].score))
+
+
+  var score_trans = new transformation(score_options)
+
+
+  score.attr('transform',score_trans.text())
+
+  var wheel_trans = new transformation(wheel_options)
+
+
+  var circle_drag = function(d) {
+
+    //shift for translate
+    d3.event.x -= wheel_options.translate.x
+    d3.event.y -= wheel_options.translate.y
+
+
+    // //get angle from g center to mouse         
+
+    var start_angle = Math.atan2(d3.event.y - d3.event.dy, d3.event.x - d3.event.dx);
+    var end_angle = Math.atan2(d3.event.y, d3.event.x);
+    var radian_diff = end_angle - start_angle;
+    var degree_diff = Math.round(arc2deg(radian_diff))
+
+
+    // if (degree_diff != 0) {
           
-      var interval = twoPI/circles.length
-      
-      var radius = 100;
-      
-      var color =  d3.scale.category10();
-      
-      var arc2deg = function (x) {
-        return x !=0 ? 180*(x/Math.PI) : 0
-      }
-
-
-      var shift = {
-        x:window.innerWidth/2,
-        y:window.innerHeight/2,
-        text: function (){
-        return this.x+','+this.y
-      },}
-
-      
-      var circle_drag =  function(d) {
+      var new_rotate =  wheel_options.rotate + degree_diff;
         
-        //shift for translate
-        d3.event.x -= shift.x
-        d3.event.y -= shift.y
-        
-        // save rotation in degrees
-        circle_drag.rotation= circle_drag.rotation || 0;
-        
-        // //get angle from g center to mouse         
+      wheel_options.rotate = new_rotate >= 0 ? new_rotate%360 : 360-(new_rotate%360)
+      
+      wheel.attr('transform', wheel_trans.text())
+  
+  
+      var category = wheel_position(wheel_options.rotate)
+      d3.select('#score').select('text') .text(scores(category.score))
+      
+      
+    // }
 
-        var start_angle = Math.atan2(d3.event.y-d3.event.dy, d3.event.x-d3.event.dx);
-        var end_angle = Math.atan2(d3.event.y, d3.event.x);
-        var radian_diff = end_angle-start_angle;
-        var degree_diff = arc2deg(radian_diff)
-      
-        circle_drag.rotation += degree_diff 
-                
-        wheel.attr('transform','translate('+shift.text()+') scale(1.5) rotate('+circle_drag.rotation+')')
-            
-      };
-      
-      var rotate = d3.behavior.drag()
-          // .origin(function(d,i) { return {x:150, y:150}; })
-          .on("drag", circle_drag);
-      
-      var wheel = svg.append('g')
-        .attr('id','wheel')
-        .call(rotate)
+  };
 
-      
-      var arc = d3.svg.arc()
-        .outerRadius(100)
-        .innerRadius(50)
-        .startAngle(function(d,i){
-            return i*interval;
-        })
-        .endAngle(function(d,i){
-            return (i+1)*interval;
-        });
-      
-      
-      
-      
-      var arcs = wheel.selectAll('g.arcs')
-        .data(circles)
-        .enter() 
-        .append('g')
-          .attr('class','arcs dont_select')
-          
-        
-      arcs.append("svg:path")
-        .attr({
-          id: function (d,i){return 'path'+i},
-          d:arc,
-          fill:function(d,i){return color(i);}
-        })
-        
-        
-    arcs.append('text')
-      .attr('class','dont_select')
-      .append("textPath")
-        .attr("xlink:href", function (d,i) {return '#path'+i})
-        .text(function (d) {return d.label})
-    
-    
-    wheel.attr('transform','translate('+shift.text()+') scale(1.5)');
+  var rotate = d3.behavior.drag()
+    .on("drag", circle_drag);
+
+  var wheel = svg.append('g')
+    .attr('id', 'wheel')
+    .call(rotate)
 
 
+  var arc = d3.svg.arc()
+    .outerRadius(100)
+    .innerRadius(20)
+    .startAngle(function(d, i) {
+      return i * interval;
+    })
+    .endAngle(function(d, i) {
+      return (i + 1) * interval;
     });
 
-  
+  var arcs = wheel.selectAll('g.arcs')
+    .data(circles)
+    .enter()
+    .append('g')
+    .attr('class', 'arcs dont_select')
+
+
+  arcs.append("svg:path")
+    .attr({
+      id: function(d, i) {
+        return 'path' + i
+      },
+      d: arc,
+      fill: function(d, i) {
+        return color(i);
+      }
+    })
+
+
+  arcs.append('text')
+    .attr('class', 'dont_select')
+    .append("textPath")
+    .attr("xlink:href", function(d, i) {
+      return '#path' + i
+    })
+    .attr('transform', 'translate(20,20)')
+    .text(function(d) {
+      return d.label
+    })
+
+
+  wheel.attr('transform', wheel_trans.text());
+
+
+});
